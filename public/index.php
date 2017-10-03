f88f6dd409104ea261043b6af69effa140b65309
mitalimn <mitalinaik@yahoo.com>
2017-10-03 11:32:19 -0500

    create page

--- public/index.php
+++ public/index.php
@@ -1,23 +1,8 @@
-<!doctype html>
-<html lang="en">
+<?php include "templates/header.php"; ?>
 
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="x-ua-compatible" content="ie=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+<ul>
+	<li><a href="create.php"><strong>Create</strong></a> - add a user</li>
+	<li><a href="read.php"><strong>Read</strong></a> - find a user</li>
+</ul>
 
-	<title>Simple Database App</title>
-
-	<link rel="stylesheet" href="css/style.css">
-</head>
-
-<body>
-	<h1>Simple Database App</h1>
-
-	<ul>
-		<li><a href="create.php"><strong>Create</strong></a> * Add a user</li>
-		<li><a href="read.php"><strong>Read</strong></a> * Find a user</li>
-	</ul>
-
-</body>
-</html>
+<?php include "templates/footer.php"; ?>
